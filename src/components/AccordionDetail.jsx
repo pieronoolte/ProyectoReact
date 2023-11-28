76865fe7c8943a8b15560475d7309854abb3da4a
Piero Nolte <pieronolte@gmail.com>
2023-11-27 20:05:40 -0500

    f5

--- src/components/AccordionDetail.jsx
+++ src/components/AccordionDetail.jsx
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Accordion from 'react-bootstrap/Accordion';
 import { ItemCount } from './ItemCount'
 
+
 export const AccordionDetail = () => {
   return (
     <Accordion className='m-5'>
